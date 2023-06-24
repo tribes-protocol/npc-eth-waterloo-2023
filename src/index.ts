f22612bd9dcf7fcc01d291b23b6f55d88d876368
zmorsy <zmorsy@umich.edu>
2023-06-23 22:21:27 -0400

    created db store class

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,7 @@ import { SearchableMessage } from "./database/types"
 
 
 async function main() {
-  console.log('Hello NPC World!')
-
-  const store = new Store('zane')
-
-  const model: SearchableMessage = {
-    id: '1',
-    author: 'zane',
-    content: 'my first db project',
-    timestamp: Date.now()
-  }
-
-  await store.put(model)
-
-  const results = await store.search('project')
-
-  console.log(`results`, results)
+// 
 }
 
 main()
